f158cec5f1aa86d75899c1531cbd3eba9f6efd52
Ewiniar <luo1172837419@icloud.com>
2023-06-13 09:36:44 -0500

    Initial Commit

---
+++ PartyExplorer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PartyExplorer
+//
+//  Created by 罗贤甫 on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PartyExplorer/PartyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyExplorerApp.swift
+//  PartyExplorer
+//
+//  Created by 罗贤甫 on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
